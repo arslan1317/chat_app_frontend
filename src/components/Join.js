6062c1ef10d14c783410b3d10c6e8cea1c893c45
marsalan <marsalan@intellexalsolutions.com>
2020-09-20 05:02:47 +0500

    project setup with join and chat components

--- src/components/Join.js
+++ src/components/Join.js
@@ -12,7 +12,7 @@ const Join = () => {
         </h1>
         <div><input type='text' placeholder="Name" className="joinInput" onChange={(event) => setName(event.target.value)}/></div>
         <div><input type='text' placeholder="Room" className="joinInput mt-20" onChange={(event) => setRoom(event.target.value)}/></div>
-        <Link to={`/chat?name=${name}&room=${room}`}>
+        <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
           <button className="button mt-20" type="submit">Sign In</button>
         </Link>
       </div>
